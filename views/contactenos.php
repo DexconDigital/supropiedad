d38eabe8ef5c9eed4aa5e4d3ce9806ebc6741b25
sebastian <me@sebastiancastiblanco.com>
2019-10-07 22:15:28 -0500

    cambio en el mapa

--- views/contactenos.php
+++ views/contactenos.php
@@ -272,7 +272,7 @@ include 'layout/menu2.php';
                     <div class="row justify-content-center" id="formulario">
 
                         <div class="col-12">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.5588300344357!2d-74.05822548590994!3d4.672451843204494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9af372dec733%3A0xbfee7a31566cf0c6!2sCra.+18+%2386A-14%2C+Bogot%C3%A1!5e0!3m2!1ses!2sco!4v1563976479016!5m2!1ses!2sco" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.5474923451197!2d-74.05741368573688!3d4.6744501431873955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9af2e8e9a9eb%3A0x4ac3417faacef1fc!2sCra.%2017%20%2388-23%2C%20Bogot%C3%A1!5e0!3m2!1ses-419!2sco!4v1570504366085!5m2!1ses-419!2sco" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                         </div>
                         <div class="col-12">
                             <h5 class="text-center">¿Tienes dudas o inquietudes? Por favor llena este
